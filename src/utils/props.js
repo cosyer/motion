e6983b7406397e3a88adf0915ac7c7cbc67a161d
chenyu <535509852@qq.com>
2019-03-23 23:36:24 +0800

    更改时间

--- src/utils/props.js
+++ src/utils/props.js
@@ -1,6 +1,6 @@
 module.exports = {
   name: "Ant Design Admin",
-  footerText: "Ant Design Admin 版权所有 © 2018 由 cosyer 支持",
+  footerText: "Ant Design Admin 版权所有 © 2019 由 cosyer 支持",
   logoSrc: "http://dir.mydearest.cn/motion/image/react.png",
   logoText: "Antd Admin",
   needLogin: true,
